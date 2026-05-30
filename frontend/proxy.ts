173a3086c0f54d2cdc3f6cbd84e0ca9981b24dc7
yarnwit <ywechramy@gmail.com>
2026-05-30 23:10:24 +0700

    แก้บัคควายๆ

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const currentPath = request.nextUrl.pathname;
 
   // อ่าน accessToken และ user จาก cookie
