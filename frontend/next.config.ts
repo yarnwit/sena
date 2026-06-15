2d2d375ff9a0f05761b9d79955f88f029f697f27
BossPrimeKub <thanachotprime@gmail.com>
2026-06-15 21:01:30 +0700

    npm run release ครับ

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true,
+  output: "standalone",
 };
 
 export default nextConfig;
-
